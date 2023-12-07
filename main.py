00ffbcd39b5baaa37ae4c55524fe4e48de252a21
Almaz <149729678+txedax@users.noreply.github.com>
2023-12-08 05:45:43 +0600

    replaced pytube with yt-dlp
    pytube complained that you had to be logged in to download some files.

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.types import FSInputFile
-from pytube import YouTube
 from dotenv import load_dotenv
+from yt_dlp import YoutubeDL
 
 load_dotenv()
 
@@ -16,15 +16,32 @@ dp = Dispatcher()
 bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
 
 
+def download_mp3(url):
+    current_directory = os.getcwd()
+
+    ydl_opts = {
+        "outtmpl": current_directory + "/downloads/%(title)s.%(ext)s",
+        "format": "m4a/bestaudio/best",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+            }
+        ],
+    }
+
+    ydl = YoutubeDL(ydl_opts)
+    info = ydl.extract_info(url, download=True)
+    title = info.get("title", "Title is not available.")
+
+    return title
+
+
 @dp.message()
 async def echo_handler(message: types.Message) -> None:
-    yt = YouTube(message.text)
-    video = yt.streams.filter(only_audio=True).first()
-    output_file = video.download(output_path="downloads")
-    new_file = output_file.replace(".mp4", ".mp3")
-    os.rename(output_file, new_file)
+    audio_title = download_mp3(message.text)
 
-    audio = FSInputFile(new_file)
+    audio = FSInputFile(f"downloads/{audio_title}.mp3")
 
     try:
         await bot.send_audio(chat_id=message.chat.id, audio=audio)
